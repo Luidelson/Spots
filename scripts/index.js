a092ce8bd054999507eff7711fc0a1332fd106a6
Lutrunks <lutrunks11@gmail.com>
2025-02-12 19:30:25 -0700

    fix form

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,19 @@ const initialCards = [
     },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector(".profile__edit-btn");
+
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editModalCloseButton = document.querySelector(".modal__close-btn");
+
+function openModal() {
+    editProfileModal.classList.add("modal_opened");
+}
+
+function closeModal() {
+    editProfileModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+editModalCloseButton.addEventListener("click", closeModal);
